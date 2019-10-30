33419cf4334e25efe8ee01004af8ccf6b60c2fd9
alvesdario <dario.alves@fatec.sp.gov.br>
2019-10-29 21:14:13 -0300

    upload some features

--- quickstart.py
+++ quickstart.py
@@ -1,39 +1,26 @@
 from __future__ import print_function
-import pickle
-import os.path
-from googleapiclient.discovery import build
+import os.path, pickle
+import dao.document as DAODOC
+
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
 
+
+# The ID of a sample document.
+DOCUMENT_ID = '1_4vWAKWhwPjGz_Bw0MTpPqcQE6PI-vK5ys3KnQLCRUs'
 
 SCOPES = ['https://www.googleapis.com/auth/documents',
     'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/drive.file'
 ]
 
-# The ID of a sample document.
-DOCUMENT_ID = '1_4vWAKWhwPjGz_Bw0MTpPqcQE6PI-vK5ys3KnQLCRUs'
+def readAllFile():
+    return document.get('body')['content'][1]['paragraph']['elements'][0]['textRun']['content']
 
-def main():
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('docs', 'v1', credentials=creds)
-
-    # Retrieve the documents contents from the Docs service.
-    document = service.documents().get(documentId=DOCUMENT_ID).execute()
-    # text1 = open('./teste1', 'r').read()
+def writeFile():
+    return None
+        # text1 = open('./teste1', 'r').read()
     # requests = [
     #      {
     #         'insertText': {
@@ -47,11 +34,28 @@ def main():
 
     # result = service.documents().batchUpdate(
     #     documentId=DOCUMENT_ID, body={'requests': requests}).execute()
-    
+
+def main():
+    creds = None
+    if os.path.exists(TOKEN_PATH):
+        with open(TOKEN_PATH, 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(TOKEN_PATH, 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('docs', 'v1', credentials=creds)
+
+    # Retrieve the documents contents from the Docs service.
+    document = service.documents().get(documentId=DOCUMENT_ID).execute()
+
     print('The title of the document is: {}'.format(document.get('title')))
-    encoded_string=document.get('body')['content'][1]['paragraph']['elements'][0]['textRun']['content']
-    with open('./saidab64', 'w+') as sai: 
-        sai.write(encoded_string)
 
 if __name__ == '__main__':
     main()
--- utils/b64.py
+++ utils/b64.py
@@ -1,31 +1,17 @@
-import sys, base64, os, math
-
-MEMORY_MAX = 15
-MEMORY_MAX_MB = MEMORY_MAX * 1048576
+import sys, base64
 
 def help():
     print("usage: b64.py <command> [<arg1>] [<arg2>]\n\tencode\tEncode a file to another file.\n\tdecode\tDecode a file to another file.")
 
-def printPercentage(atual, fim):
-    status = atual/fim
-    sys.stdout.write('\r')
-    sys.stdout.write(f'{math.floor(status*100)}% {atual}/{fim}')
-    sys.stdout.flush()
 
 def encod(entrada, saida):
     encoded_string=""
-    originalFile = open(entrada, "rb")
-    iterations = math.ceil(os.stat(sys.argv[2]).st_size/MEMORY_MAX_MB)
-    print(f'you will have {iterations} iterations')
-    for i in range(0,iterations):
-        originalFile.seek(i*MEMORY_MAX_MB)
-        encoded_string = originalFile.read(MEMORY_MAX_MB)
-        resultFile = open(saida+str(i), "wb")
+    with open(entrada, "rb") as originalFile:
+        encoded_string = base64.b64encode(originalFile.read())
+        originalFile.close()
+    with open(saida, "wb") as resultFile:
         resultFile.write(encoded_string)
-        resultFile.close()
-        printPercentage(i,iterations)
-    originalFile.close()
-    print("all files encoded sucessfuly!")
+    print("encoded sucessfuly!")
 
 def decod(entrada, saida):
     encoded_string=""
