855ccaaeb46271c9a807a4b0d0ff7485467ca5a8
alvesdario <dario.alves@fatec.sp.gov.br>
2020-01-31 00:08:49 -0300

    fix credsgen.js

--- services/credsgen.js
+++ services/credsgen.js
@@ -2,51 +2,17 @@ const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
 
-const SCOPES = ['https://www.googleapis.com/auth/documents',
-'https://www.googleapis.com/auth/drive',
-'https://www.googleapis.com/auth/drive.file'
-];
-
+const SCOPES = require('../settings.json').SCOPES;
 const TOKEN_PATH = 'token.json';
 
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  const credentials = JSON.parse(content);
-
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) {
-        const authUrl = oAuth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: SCOPES,
-          });
-          console.log('Authorize this app by visiting this url:', authUrl);
-          const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-          });
-          rl.question('Enter the code from that page here: ', (code) => {
-            rl.close();
-            oAuth2Client.getToken(code, (err, token) => {
-              if (err) return console.error('Error retrieving access token', err);
-              oAuth2Client.setCredentials(token);
-
-              fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-              });
-            });
-          });
-        // return getNewToken(oAuth2Client);
-    }
-    oAuth2Client.setCredentials(JSON.parse(token));
-  });
-
-//   authorize(JSON.parse(content));
-});
+module.exports = {
+  generateCred: () => {
+    fs.readFile('credentials.json', (err, content) => {
+      if (err) return console.log('Error loading client secret file:', err);
+      authorize(JSON.parse(content));
+    });
+  }
+}
 
 function authorize(credentials) {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
@@ -54,34 +20,10 @@ function authorize(credentials) {
       client_id, client_secret, redirect_uris[0]);
 
   fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) {
-        const authUrl = oAuth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: SCOPES,
-          });
-          console.log('Authorize this app by visiting this url:', authUrl);
-          const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-          });
-          rl.question('Enter the code from that page here: ', (code) => {
-            rl.close();
-            oAuth2Client.getToken(code, (err, token) => {
-              if (err) return console.error('Error retrieving access token', err);
-              oAuth2Client.setCredentials(token);
-
-              fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-              });
-            });
-          });
-        // return getNewToken(oAuth2Client);
-    }
+    if (err) return getNewToken(oAuth2Client);
     oAuth2Client.setCredentials(JSON.parse(token));
   });
 }
-
 
 function getNewToken(oAuth2Client) {
   const authUrl = oAuth2Client.generateAuthUrl({
